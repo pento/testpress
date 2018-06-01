c3ddea23440c31d234981a114398d1547825d3c6
Gary Pendergast <gary@pento.net>
2018-06-01 23:52:18 +1000

    Get `npm install` working on Windows.

--- src/services/constants.js
+++ src/services/constants.js
@@ -5,6 +5,7 @@ const TOOLS_DIR = normalize( app.getPath( 'userData' ) + '/tools' );
 const ARCHIVE_DIR = normalize( TOOLS_DIR + '/archives' );
 
 const NODE_DIR = normalize( TOOLS_DIR + '/node' );
+const NPM_CACHE_DIR = normalize( ARCHIVE_DIR + '/npm-cache' );
 
 const nodeBin = ( 'win32' === process.platform ) ? '/node.exe' : '/bin/node';
 const npmBin = ( 'win32' === process.platform ) ? '/node_modules/npm/bin/npm-cli.js' : '/bin/npm';
@@ -12,11 +13,11 @@ const npmBin = ( 'win32' === process.platform ) ? '/node_modules/npm/bin/npm-cli
 const NODE_BIN = normalize( NODE_DIR + nodeBin );
 const NPM_BIN = normalize( NODE_DIR + npmBin );
 
-
 module.exports = {
 	TOOLS_DIR,
 	ARCHIVE_DIR,
 	NODE_DIR,
+	NPM_CACHE_DIR,
 	NODE_BIN,
 	NPM_BIN,
 };
