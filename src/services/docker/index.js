0b523d164cc694761179a3d30046ce0cb7d9cc0e
Gary Pendergast <gary@pento.net>
2019-03-29 13:34:10 +1100

    Copy the PHP config files to the tools directory on startup.

--- src/services/docker/index.js
+++ src/services/docker/index.js
@@ -159,6 +159,8 @@ async function startDocker() {
 	writeFileSync( normalize( TOOLS_DIR + '/docker-compose.scripts.yml' ), scriptOptionsYaml );
 
 	copyFileSync( normalize( __dirname + '/default.conf' ), normalize( TOOLS_DIR + '/default.conf' ) );
+	copyFileSync( normalize( __dirname + '/php-config.ini' ), normalize( TOOLS_DIR + '/php-config.ini' ) );
+	copyFileSync( normalize( __dirname + '/phpunit-config.ini' ), normalize( TOOLS_DIR + '/phpunit-config.ini' ) );
 
 	if ( USING_TOOLBOX ) {
 		await startDockerMachine();
