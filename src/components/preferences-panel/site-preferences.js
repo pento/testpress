07842180d9f6b411009f19a0b01f1c7c3466912c
Daniel Richards <daniel.richards@automattic.com>
2019-04-02 16:45:22 +0800

    Use @wordpress/components for the site preferences panel

--- src/components/preferences-panel/site-preferences.js
+++ src/components/preferences-panel/site-preferences.js
@@ -3,11 +3,19 @@
  */
 import React from 'react';
 
+/**
+ * WordPress dependencies
+ */
+import { BaseControl } from '@wordpress/components';
+
 export default function SitePreferences( { port, onPortChange, onPortInputBlur } ) {
 	return (
 		<div className="preferences-panel">
-			<div className="preferences-panel__input preferences-panel__input--short preferences-panel__input--inline">
-				<label htmlFor="preferences-port">Port Number:</label>
+			<BaseControl
+				id="preferences-port"
+				className="preferences-panel__input"
+				label="Port Number"
+			>
 				<input
 					type="number"
 					id="preferences-port"
@@ -15,8 +23,11 @@ export default function SitePreferences( { port, onPortChange, onPortInputBlur }
 					onChange={ ( event ) => onPortChange( event.target.value ) }
 					onBlur={ onPortInputBlur }
 				/>
+			</BaseControl>
+			{ /* <div className="preferences-panel__input preferences-panel__input--short preferences-panel__input--inline">
+				<label htmlFor="preferences-port">Port Number:</label>
 
-			</div>
+			</div> */ }
 		</div>
 	);
 }
